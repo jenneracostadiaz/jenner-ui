f9588918bd0a022bd31fc8d128546d3e2cbf95fc
Jenner Acosta Díaz <my@jenner.pe>
2022-11-17 12:56:22 -0500

    Header basic structure

--- header.php
+++ header.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> >
-  <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-    <?php wp_head(); ?>
-  </head>
-  <body <?php body_class(); ?> >
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="profile" href="http://gmpg.org/xfn/11" />
+  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+  <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?> >
+  <header class="main-header">
+    <div class="container">
+      <?php 
+        if ( function_exists( 'the_custom_logo' ) ) {
+          echo '<div class="main-header__brand">';
+            the_custom_logo();
+          echo '</div>';
+        }
+      ?>
+    </div>
+  </header>
