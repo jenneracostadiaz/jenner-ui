027726585cbe3853cbbdb3e3a9e25202972e9d8b
Jenner Acosta Díaz <my@jenner.pe>
2022-11-17 13:31:36 -0500

    Update Index Contianer

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 get_header();
-
-if ( have_posts() ) :
+ 
+if ( have_posts() ) : 
     while ( have_posts() ) : the_post();
-        the_title( '<h1>', '</h1>' );
-        the_content();
+        ?>
+        <main class="container">
+            <?php 
+                the_title( '<h1>', '</h1>' ); 
+                the_content();
+            ?>
+        </main>
+        <?php
     endwhile;
 else :
     get_template_part( 'template-parts/page', 'error' );
 endif;
-
+ 
 get_footer();
 ?>
