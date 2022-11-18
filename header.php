9a394c96637ea69e0ad2f1a30350bd63ac819c73
Jenner Acosta Díaz <my@jenner.pe>
2022-11-18 01:52:18 -0500

    Darkmode HTML Structure

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 </head>
 <body <?php body_class(); ?> >
   <header class="main-header">
-    <div class="container">
+    <nav class="main-header__container container">
       <?php 
         if ( function_exists( 'the_custom_logo' ) ) {
           echo '<div class="main-header__brand">';
@@ -20,7 +20,7 @@
       ?>
       <button onclick="themeMode()" class="btn-theme-mode">
         <svg class="svg-icon btn-theme-mode__svg"><use href="<?php echo get_template_directory_uri( ) ?>/assets/icons/symbols.svg#sun" /></svg>
-        <div class="btn-theme-mode__txt-switch txt-switch">Modo <span class="txt-switch__light">Claro</span> / <span class="txt-switch__dark">Oscuro</span></div>
+        <p class="btn-theme-mode__txt-switch txt-switch">Modo <span class="txt-switch__light">Claro</span> / <span class="txt-switch__dark">Oscuro</span></p>
       </button>
-    </div>
+    </nav>
   </header>
