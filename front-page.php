2cce862ced12dff7c74e66a364dba03f0e311494
Jenner Acosta Díaz <my@jenner.pe>
2022-11-18 16:24:00 -0500

    Cover Image

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main class="home-page container">
+    <section class="cover">
+        <div class="cover__image">
+            <picture> 
+                <source srcset='http://jennerui.local/wp-content/uploads/2022/11/dog-cover-768x816.png' media='(min-width: 480px)'>
+                <img src='http://jennerui.local/wp-content/uploads/2022/11/dog-cover-282x300.png' alt='Dog Coder Espacial'> 
+             </picture>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
